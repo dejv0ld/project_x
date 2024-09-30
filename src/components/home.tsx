e737ac34f8a0f3c527ee1c1f48f7a426e890d69f
dejv0ld <david_johnson_1992@hotmail.com>
2024-09-30 23:35:54 +0200

    setting upp the project template

---
+++ src/components/home.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+const Homejanne: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+    </div>
+  );
+};
+
+export default Homejanne;
